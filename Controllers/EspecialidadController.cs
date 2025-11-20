db9c206d0c5a05116c22d1ec40735baf3288ddfc
Lautaro Berca <lautaberca2005@gmail.com>
2025-11-20 12:39:42 -0300

    Bugfix on selecting a specialty when registering

--- Controllers/EspecialidadController.cs
+++ Controllers/EspecialidadController.cs
@@ -1,1 +1,20 @@
-//a eliminar
+
+using MedCenter.Services.EspecialidadService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedCenter.Controllers;
+public class EspecialidadController : BaseController
+{
+    private readonly EspecialidadService e;
+
+    public EspecialidadController(EspecialidadService especialidadService)
+    {
+        e = especialidadService;
+    }
+
+    public async Task<IActionResult> GetEspecialidadesJson()
+    {
+        var especialidades = await e.GetEspecialidades();
+        return Json(especialidades);
+    }
+}
